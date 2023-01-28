6c184616926a82a75f8867cc41576646605fc478
Nithin U <106614289+NithinU2802@users.noreply.github.com>
2023-01-28 23:52:53 +0530

    Create ValidBracket.java

---
+++ ValidBracket.java
@@ -0,0 +1,47 @@
+/******************************************************************************
+
+Check whether the given string has valid brackets or not.
+
+Input: {[]}
+
+Output: Yes
+
+Input: {[}]
+
+Output: No 
+
+Input: {[()]}{}
+
+Output: Yes
+
+*******************************************************************************/
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+	Scanner x=new Scanner(System.in);
+	String s=x.nextLine();
+	int r=-1;
+	char[] st=new char[s.length()];
+	for(int i=0;i<s.length();i++){
+	    char a=s.charAt(i);
+	   if(r==-1){
+	       if(a=='}' || a==']' || a=='}'){
+	           System.out.print("No");
+	           return;
+	       }else st[++r]=a;
+	   }else if(a=='}' && st[r]=='{') r--;
+	   else if(a==']' && st[r]=='[') r--;
+	   else if(a==')' && st[r]=='(') r--;
+	   else if(a=='{' || a=='[' || a=='{') st[++r]=a;
+	   else{
+	       System.out.print("No");
+	       return;
+	   }
+	   }
+	   System.out.print("Yes");
+	
+	}
+}
+
