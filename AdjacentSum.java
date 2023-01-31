152c6a0869fc8840942c7fa13b75ea9bcd38d7b4
Nithin U <106614289+NithinU2802@users.noreply.github.com>
2023-01-31 23:56:19 +0530

    Create AdjacentSum.java

---
+++ AdjacentSum.java
@@ -0,0 +1,30 @@
+/******************************************************************************
+
+Sum with right adjacent element....
+
+Input: 1 2 3 4 5 6
+
+Output: 3 7 11
+
+
+Input: 1 2 3
+
+Output: 3 3
+
+*******************************************************************************/
+import java.util.*;
+
+public class Main
+{
+    
+	public static void main(String[] args) {
+	Scanner x=new Scanner(System.in);
+	String[] s=x.nextLine().split(" ");
+	int[] a=new int[s.length];
+	int n=0;
+	for(String i: s) a[n++]=Integer.parseInt(i);
+	for(int i=0;i+1<n;i+=2)
+	System.out.print((a[i]+a[i+1])+" ");
+	if(n%2!=0) System.out.print(a[n-1]);
+	}
+}
