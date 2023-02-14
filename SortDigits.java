78c146680e4583d8c70415783408e517234a3fa4
Nithin U <106614289+NithinU2802@users.noreply.github.com>
2023-02-14 23:23:54 +0530

    Update SortDigits.java

--- SortDigits.java
+++ SortDigits.java
@@ -11,13 +11,13 @@ Input: 38627
 Output: 23678
 
 *******************************************************************************/
-
+import java.util.*;
 public class Main
 {
     
     public static void sort(int a[]){
         int n=a.length;
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n-1;i++){
             if(a[i]>a[i+1]){
                 int t=a[i];
                 a[i]=a[i+1];
@@ -29,9 +29,9 @@ public class Main
     
 	public static void main(String[] args) {
 	Scanner x=new Scanner(System.in);
-	int n=x.nextLine();
-	int[] a=new int[n.length];
-	int t=n;,k=0;
+	String n=x.nextLine();
+	int[] a=new int[n.length()];
+	int t=Integer.parseInt(n),k=0;
 	while(t!=0){
 	    a[k++]=t%10;
 	    t/=10;
