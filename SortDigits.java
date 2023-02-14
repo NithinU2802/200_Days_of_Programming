c8b52ef6f17e24bd211abf3796cd836106f1457f
Nithin U <106614289+NithinU2802@users.noreply.github.com>
2023-02-14 23:20:27 +0530

    Create SortDigits.java

---
+++ SortDigits.java
@@ -0,0 +1,44 @@
+/******************************************************************************
+
+Sort the given digit and create a digit.....
+
+Input: 4564
+
+output: 4456
+
+Input: 38627
+
+Output: 23678
+
+*******************************************************************************/
+
+public class Main
+{
+    
+    public static void sort(int a[]){
+        int n=a.length;
+        for(int i=0;i<n;i++){
+            if(a[i]>a[i+1]){
+                int t=a[i];
+                a[i]=a[i+1];
+                a[i+1]=t;
+                i=-1;
+            }
+        }
+    }
+    
+	public static void main(String[] args) {
+	Scanner x=new Scanner(System.in);
+	int n=x.nextLine();
+	int[] a=new int[n.length];
+	int t=n;,k=0;
+	while(t!=0){
+	    a[k++]=t%10;
+	    t/=10;
+	}
+	sort(a);
+	String r="";
+	for(int i: a) r+=String.valueOf(i);
+	System.out.print(r);
+	}
+}
